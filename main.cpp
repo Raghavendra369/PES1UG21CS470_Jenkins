8ffba673cdf42d4add175a33921db28fe0c3288e
Raghavendra369 <rsb3186009@gmail.com>
2024-03-08 20:17:07 +0530

    added a cpp file

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  cout<<"I was burning"<<endl;
+  cout<<"But instead you came to complain me about the smell and ashes"<<endl;
+
+  return 0;
+}
